bfa18aff7d73c8f0c879c13136da8c3cdba8fe69
leoTav <65578088+Leon1618@users.noreply.github.com>
2021-11-08 23:09:56 +1100

    Update homework.js

--- class00/homework.js
+++ class00/homework.js
@@ -1,1 +1,69 @@
+// Convert a string into a number?
 
+// let num = "4";
+// num = parseInt(num);
+// console.log(num);
+
+//Bonus: 
+
+// let num2 = "5.6"
+// num2 = parseFloat(num2)
+// console.log(num2);
+
+// Convert a number into a string?
+
+// let num = 15;
+// console.log(typeof num);
+// num = num.toString();
+// console.log(num);
+// console.log(typeof num);
+
+// Make a string uppercased?
+
+// let greet = "hello";
+// //greet = greet.toUpperCase();
+// console.log(greet);
+
+// // Bonus: Make just the first character uppercased!
+
+// let firstLetter = greet.charAt(0);
+// console.log(firstLetter);
+// firstLetter = firstLetter.toUpperCase();
+// console.log(firstLetter);
+
+// // Get the second character out of a string?
+
+// let word = "hello";
+// secondLetter = word.charAt(1);
+// console.log(secondLetter);
+
+// Use concatenation to combine two strings (e.g. join "Hello " and "World")?
+
+// let firstName = "Jacques";
+// let lastName = "Cousteau";
+// let fullName = firstName  + " " + lastName
+// console.log(fullName);
+
+// Bonus: Try using the prompt method to get the user's firstName and lastName - then combine them
+
+// let userName = prompt("Enter your first name:");
+// let userLastName = prompt("Enter your last name:");
+// let fullName = userName + " " + userLastName;
+// console.log(fullName);
+
+// Get a number and square it (e.g. 4 squared is 16)?
+
+// let num = 16;
+// num = Math.cbrt(num);
+// console.log(num);
+
+// // Bonus: Do the same thing but cube it
+
+// cubeRt = num * num * num;
+// console.log(cubeRt);
+
+// // 7. Get the square root of a number
+
+// let num = 25;
+// numSq = Math.sqrt(num);
+// console.log(numSq);
