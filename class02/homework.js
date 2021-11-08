8abc88b572bfe1cf4a457aed18681fb2c1e6c49a
leoTav <65578088+Leon1618@users.noreply.github.com>
2021-11-08 22:54:26 +1100

    Update homework.js

--- class02/homework.js
+++ class02/homework.js
@@ -1,1 +1,39 @@
+// 1. Log every number from 0 to 10
 
+// let num = 0;
+// for (num = 0; num <= 10; num += 1) {
+//   console.log(num);
+// }
+
+// 2. Log every number from 4 to -16
+
+// let num = 4;
+// for (num = 4; num >= -16; num -= 1) {
+//   console.log(num);
+// }
+
+// 3. Log every fourth number from 8 to 41
+
+// let num = 8;
+
+// for (num = 8; num <= 41; num += 4) {
+//   console.log(num);
+// }
+
+// 4. The Classic Fizzbuzz Program
+// Loop from 0 to 100.
+// If the number is evenly divisible by 3, log the number and "Fizz"
+// If the number is evenly divisible by 5, log the number and "buzz"
+// If the number is evenly divisible by both 3 AND 5, log the number and "Fizzbuzz"
+
+// let num = 1;
+
+// for (num = 1; num <= 100; num += 1) {
+//   if (num % 3 === 0 && num % 5 === 0) {
+//     console.log(num + " Fizzbuzz");
+//   } else if (num % 5 === 0) {
+//     console.log(num + " buzz");
+//   } else if (num % 3 === 0) {
+//     console.log(num + " Fizz");
+//   }
+// }
