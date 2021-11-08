9fca5f8552b2ea9484ebff65da2f6f0c9c0b1265
leoTav <65578088+Leon1618@users.noreply.github.com>
2021-11-08 22:39:24 +1100

    Add files via upload

---
+++ class01/class01_data_types.js
@@ -0,0 +1,114 @@
+// Oct 19th (Tuesday) - Lesson 1 (Data Types)
+
+//     Recording: https://generalassembly.zoom.us/rec/share/WLMTz_j7aNI9U3fc0KK-LUeQl4KyBgdVYB52pYF3Rff1XFewaJSsybVBKdEZE7nS.le__t0Hji4RRuH4G
+//     The passcode is sO3tH^67 (edited)
+
+//     Not much homework tonight, just a little bit of reading:
+//     https://javascript.info/types
+//     https://javascript.info/variables
+//     https://eloquentjavascript.net/01_values.html
+
+//     If you go through the slides (which you can find here: https://github.com/ga-wolf/jsdr-06/blob/main/class_00/slides/slides.pdf) and want to have a crack at some of Thursday's homework, you can try the exercises found here: https://github.com/ga-wolf/jsdr-06/blob/main/class_00/exercises/00_primitive_data_types_and_variables.md
+
+//     All of my code from tonight can be found here: https://github.com/ga-wolf/jsdr-06/tree/main/class_00/code
+
+//     https://github.com/ga-wolf/jsdr-06/blob/main/class_00/slides/slides.pdf
+
+//     console.log(10.1);
+
+//     console.log(-15);
+
+//     // Addition
+//     10 + 2;
+
+//     // Subtraction
+//     5 - 3;
+
+//     // Multiplication
+//     5 * 10;
+
+//     // Division
+//     6 / 2;
+
+//     // Booleans - Logical Type
+//     console.log(true);
+//     console.log(false);
+
+// Exercise List (1)
+
+//     1. Convert a string into a number?
+
+//     let num = "4";
+//     num = parseInt(num);
+//     console.log(num);
+
+//     Bonus:
+
+//     let num2 = "5.6"
+//     num2 = parseFloat(num2)
+//     console.log(num2);
+
+//     2. Convert a number into a string?
+
+//     let num = 15;
+//     console.log(typeof num);
+//     num = num.toString();
+//     console.log(num);
+//     console.log(typeof num);
+
+//     3. Make a string UPPERCASED?
+
+//     let greet = "hello";
+//     greet = greet.toUpperCase();
+//     console.log(greet);
+
+//     Bonus: Make just the first character uppercased!
+
+//     let firstLetter = greet.charAt(0);
+//     console.log(firstLetter);
+//     firstLetter = firstLetter.toUpperCase();
+//     console.log(firstLetter);
+
+//     4. Get the second character out of a string?
+
+//     let word = "hello";
+//     secondLetter = word.charAt(1);
+//     console.log(secondLetter);
+
+//     5. Use concatenation to combine two strings (e.g. join "Hello " and "World")?
+
+//     let firstName = "Jacques";
+//     let lastName = "Cousteau";
+//     let fullName = firstName  + " " + lastName
+//     console.log(fullName);
+
+//     Bonus: Try using the prompt method to get the user's firstName and lastName - then combine them
+
+//     let userName = prompt("Enter your first name:");
+//     let userLastName = prompt("Enter your last name:");
+//     let fullName = userName + " " + userLastName;
+//     console.log(fullName);
+
+//     6. Get a number and square it (e.g. 4 squared is 16)?
+
+//     let num = 16;
+//     num = Math.cbrt(num);
+//     console.log(num);
+
+//     Bonus: Do the same thing but cube it
+
+//     cubeRt = num * num * num;
+//     console.log(cubeRt);
+
+//     7. Get the square root of a number
+
+//     let num = 25;
+//     numSq = Math.sqrt(num);
+//     console.log(numSq);
+
+// Nov 4th (Tuesday) - Lesson 6
+
+// Week 4
+
+// Week 5
+// Week 6
