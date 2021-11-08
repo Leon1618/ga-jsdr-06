7fe3f78a438d34cc786082914714f02ec7a841f9
leoTav <65578088+Leon1618@users.noreply.github.com>
2021-11-08 22:46:56 +1100

    Add files via upload

---
+++ class00/class00_data_types.js
@@ -0,0 +1,42 @@
+// Oct 19th (Tuesday) - Lesson 1 (Data Types)
+
+//     Recording: https://generalassembly.zoom.us/rec/share/WLMTz_j7aNI9U3fc0KK-LUeQl4KyBgdVYB52pYF3Rff1XFewaJSsybVBKdEZE7nS.le__t0Hji4RRuH4G
+//     The passcode is sO3tH^67 (edited)
+
+//     Not much homework tonight, just a little bit of reading:
+//     https://javascript.info/types
+//     https://javascript.info/variables
+//     https://eloquentjavascript.net/01_values.html
+
+//     If you go through the slides (which you can find here: https://github.com/ga-wolf/jsdr-06/blob/main/class_00/slides/slides.pdf) and want to have a crack at some of Thursday's homework, you can try the exercises found here: https://github.com/ga-wolf/jsdr-06/blob/main/class_00/exercises/00_primitive_data_types_and_variables.md
+
+//     All of my code from tonight can be found here: https://github.com/ga-wolf/jsdr-06/tree/main/class_00/code
+
+//     https://github.com/ga-wolf/jsdr-06/blob/main/class_00/slides/slides.pdf
+
+//     console.log(10.1);
+
+//     console.log(-15);
+
+//     // Addition
+//     10 + 2;
+
+//     // Subtraction
+//     5 - 3;
+
+//     // Multiplication
+//     5 * 10;
+
+//     // Division
+//     6 / 2;
+
+//     // Booleans - Logical Type
+//     console.log(true);
+//     console.log(false);
+
+// Nov 4th (Tuesday) - Lesson 6
+
+// Week 4
+
+// Week 5
+// Week 6
