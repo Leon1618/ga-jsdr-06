11be36ab6f53e1212b0bb112a365753cc115f52d
leoTav <65578088+Leon1618@users.noreply.github.com>
2021-11-26 00:05:41 +1100

    Update homework_functions_extra.js

--- class05/homework_functions_extra.js
+++ class05/homework_functions_extra.js
@@ -237,3 +237,303 @@ function travelFrom (fromStation, toStation) {
 travelFrom("cEntrAl", "CirCular quAy");
 travelFrom("CirCular quAy", "cEntrAl");
 travelFrom("Kings Cross", "cEntrAl");
+
+************************************************************************
+    
+    // A (more complicated) Transit Application
+function travelFrom (fromStation, toStation) {
+    const stations = { 
+        T1: ["Museum", "St. James", "Circular Quay", "Wynyard"],
+        T2: ["Circular Quay", "Townhall", "Central", "Redfern"],
+        T3: ["Central", "Macdonaldtown", "Newtown"],
+    };
+    
+    function track (station) {
+        let tracks = [];
+        let validStation = false;
+        for (let key in stations) {
+            if (stations[key].find(a=> a.toLowerCase() === station.toLowerCase())) {
+                tracks.push(key);
+                validStation = true;
+            }
+        }
+        if (validStation) {
+            return tracks;
+        } else return false; 
+    }
+    function transferAt () {
+        if (fromTracks.includes("T1") && toTracks.includes("T1")){
+            fromTracks = ["T1"];
+            toTracks = ["T1"];
+            return false;
+        }
+        if (fromTracks.includes("T2") && toTracks.includes("T2")){
+            fromTracks = ["T2"];
+            toTracks = ["T2"];
+            return false;
+        }
+        if (fromTracks.includes("T3") && toTracks.includes("T3")){
+            fromTracks = ["T3"];
+            toTracks = ["T3"];
+            return false;
+        }
+        if ((fromTracks.includes("T1") && toTracks.includes("T2")) || (toTracks.includes("T1") && fromTracks.includes("T2"))){
+            if (fromTracks.indexOf("T3") > -1){
+                fromTracks.splice(fromTracks.indexOf("T3"),1);
+            }
+            if (toTracks.indexOf("T3")> -1){
+                toTracks.splice(toTracks.indexOf("T3"),1);
+            }
+            return ["Circular Quay"];
+        }
+        if ((fromTracks.includes("T2") && toTracks.includes("T3")) || (toTracks.includes("T2") && fromTracks.includes("T3"))){
+            if (fromTracks.indexOf("T1") > -1){
+                fromTracks.splice(fromTracks.indexOf("T1"),1);
+            }
+            if (toTracks.indexOf("T1") > -1){
+                toTracks.splice(toTracks.indexOf("T1"),1);
+            }
+            return ["Central"];
+        }
+        if (fromTracks.includes("T1") && toTracks.includes("T3")){
+            return ["Circular Quay", "Central"];
+        }
+        if (toTracks.includes("T1") && fromTracks.includes("T3")){
+            return ["Central","Circular Quay"];
+        }
+    }
+    function stationIndex (station, track){
+        return (stations[track[0]].findIndex(i => i.toLowerCase() === station.toLowerCase()));
+    }
+    function travelItinerary () {
+        function trackItinerary (trackAIndex, stationAIndex, trackBIndex, stationBIndex) {
+            if (stationAIndex < stationBIndex) {
+                for (let i = stationAIndex; i <= stationBIndex; i +=1) {
+                    itinerary = itinerary + "-" + stations[trackAIndex][i] + " \n";
+                    stops += 1;
+                }
+            } else {
+                for (let i = fromStationIndex; i >= toStationIndex; i -=1) {
+                    itinerary = itinerary + "-" + stations[trackAIndex][i] + " \n";
+                    stops += 1;
+                }
+            }
+        }
+        if (!transferStation){
+            trackItinerary(fromTracks[0], fromStationIndex, toTracks[0], toStationIndex);
+        }
+        if (transferStation[0] && !transferStation[1]) {
+            let fromTransferIndex = stationIndex(transferStation[0],fromTracks);
+            let toTransferIndex = stationIndex(transferStation[0],toTracks);
+            trackItinerary(fromTracks[0], fromStationIndex, fromTracks[0], fromTransferIndex);
+            itinerary = itinerary.slice(0,-2) + ": " + "Transfer to track "+ toTracks[0] + " \n";
+            trackItinerary(toTracks[0], toTransferIndex, toTracks[0], toStationIndex);
+            stops -= 1;
+        }
+        if (transferStation[0] && transferStation[1]) {
+            let fromTransferIndex = stationIndex(transferStation[0],fromTracks);
+            let fromT2Index = stationIndex(transferStation[0],["T2"]);
+            let toT2Index = stationIndex(transferStation[1],["T2"]);
+            let toTransferIndex = stationIndex(transferStation[1],toTracks);
+            trackItinerary(fromTracks[0], fromStationIndex, fromTracks[0], fromTransferIndex);
+            itinerary = itinerary.slice(0,-2) + ": " + "Transfer to track T2 \n";
+            trackItinerary("T2", fromT2Index, "T2", toT2Index);
+            itinerary = itinerary.slice(0,-2) + ": " + "Transfer to track "+ toTracks[0] + " \n";
+            trackItinerary(toTracks[0], toTransferIndex, toTracks[0], toStationIndex);
+            stops -= 2;
+        }
+        
+        
+        console.log("your trip will require", stops, "stops");
+        console.log(itinerary);
+    }
+    let fromTracks = track(fromStation);
+    let toTracks = track(toStation);
+    
+    
+    if (!fromTracks) {
+        console.log (fromStation, "station not found")
+        return;
+    }
+    if (!toTracks) {
+        console.log (toStation, "station not found")
+        return;
+    }
+    let transferStation = transferAt();
+    let fromStationIndex = stationIndex(fromStation,fromTracks);
+    let toStationIndex = stationIndex(toStation,toTracks);
+    let itinerary = "";
+    let stops = 0;
+    
+    travelItinerary();
+}
+travelFrom("Museum", "Wynyard"); // Both on track 1 no transfer
+travelFrom("Circular Quay", "Redfern"); // Both on track 2 no transfer
+travelFrom("newtown", "Central"); // Both on track 3 no transfer
+travelFrom("MusEum", "Wynyard"); // Case insensitive
+travelFrom("Wynyard", "MusEum"); // travel backwards
+travelFrom("Kings Cross", "Wynyard"); // Halts if not a station and returns invalid station name.
+travelFrom("Museum", "Redfern"); // from track 1 to track 2 showing a transfer at "Circular Quay"
+travelFrom("redfern", "newtown") // from track 2 to track 3 showing a transfer at "central"
+travelFrom("Museum", "newtown") // from track 1 to track 3 showing a transfer at "Circular Quay" & "central"
+travelFrom("newtown", "museum") // from track 3 to track 1 showing a transfer at "central" & "Circular Quay"
+Collapse
+
+
+
+:raised_hands:
+1
+
+
+1 reply
+17 days agoView thread
+
+Benedict Lawrence  17:19
+some work on the functions problems (still more to do)
+
+Benedict Lawrence  17:19
+const users = [
+  {
+    email: "groucho@ga.co",
+    password: "chicken",
+    isAdmin: true,
+  },
+  {
+    email: "harpo@ga.co",
+    password: "elephant",
+    isAdmin: false,
+  },
+  {
+    email: "gummo@ga.co",
+    password: "pinkFairyArmadillo",
+    isAdmin: true,
+  },
+  {
+    email: "zeppo@ga.co",
+    password: "dumboOctopus",
+    isAdmin: false,
+  },
+];
+
+// get username and password
+// iterate through user array
+// if user.email and user.password are as povided log "signed in"
+// else log "Sorry, something went wrong".
+
+function signIn(username, password) {
+  let authenticated = false;
+  users.forEach(check);
+  function check(item) {
+    if (username === item.email && password === item.password) {
+      authenticated = true;
+      return authenticated;
+    }
+  }
+  if (authenticated === true) {
+    console.log("You are logged in");
+  } else {
+    console.log("Sorry, something went wrong");
+  }
+}
+
+signIn("incorrectlogin@ga.co", "incorrectpassword");
+signIn("gummo@ga.co", "pinkFairyArmadillo");
+
+function createAccount(newEmail, newPassword, newIsAdmin) {
+  users.push({
+    email: newEmail,
+    password: newPassword,
+    isAdmin: newIsAdmin,
+  });
+  console.log(newEmail, "has been created");
+}
+
+createAccount("mickeymouse@ga.co", "donaldduck", false);
+
+function deleteAccount(accountToDelete, password) {
+  let ableToDelete = false;
+  users.forEach(check);
+  function check(item, index) {
+    if (accountToDelete === item.email && password === item.password) {
+      users.splice(index, 1);
+      ableToDelete = true;
+    }
+  }
+  if (ableToDelete === true) {
+    console.log(accountToDelete, "has been deleted");
+  } else {
+    console.log("Unable to authenticate", accountToDelete, "Please try again");
+  }
+}
+
+deleteAccount("mickeymouse@ga.co", "donaldduck");
+
+const stations = [
+  "Museum",
+  "St. James",
+  "Circular Quay",
+  "Wynyard",
+  "Townhall",
+  "Central",
+  "Redfern",
+  "Macdonaldtown",
+  "Newtown",
+];
+
+//create function that receives start station and end station
+
+// iterate from start station to end station
+
+//count the iterations
+
+//print the stops
+
+function trainJourney(startStation, endStation) {
+  let standardStations = stations.map((station) => station.toLowerCase());
+
+  standardStartStation = startStation.toLowerCase();
+
+  standardEndStation = endStation.toLowerCase();
+
+  startStationIndex = standardStations.indexOf(standardStartStation);
+
+  endStationIndex = standardStations.indexOf(standardEndStation);
+
+  if (startStationIndex === -1 && endStationIndex === -1) {
+    console.log(
+      "'" + startStation + "' and '" + endStation + "' are not valid stations."
+    );
+  } else if (startStationIndex === -1) {
+    console.log("'" + startStation + "' is not a valid station.");
+  } else if (endStationIndex === -1) {
+    console.log("'" + endStation + "' is not a valid station.");
+  } else if (startStationIndex > endStationIndex) {
+    console.log(
+      "To go from '" +
+        stations[startStationIndex] +
+        "' to '" +
+        stations[endStationIndex] +
+        "' you'll need to go through " +
+        (startStationIndex - endStationIndex) +
+        " stops. Those stations are:"
+    );
+
+    for (i = startStationIndex; i >= endStationIndex; i -= 1) {
+      console.log("-", stations[i]);
+    }
+  } else {
+    console.log(
+      "To go from '" +
+        stations[startStationIndex] +
+        "' to '" +
+        stations[endStationIndex] +
+        "' you'll need to go through " +
+        (endStationIndex - startStationIndex) +
+        " stops. Those stations are:"
+    );
+
+    for (i = startStationIndex; i <= endStationIndex; i += 1) {
+      console.log("-", stations[i]);
+    }
+  }
+}
